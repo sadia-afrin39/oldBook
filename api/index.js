82464e6b7f99230664b2d749aed763ecc5a5cc67
sadia-afrin39 <tarin.cse5.bu@gmail.com>
2024-01-05 22:07:55 +0600

    setup

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.listen(port, () => {
+  console.log("Server is running on port 8000");
+});
+
+mongoose
+  .connect("mongodb+srv://SadiaAfrin:12345Tarin@cluster0.3lchog8.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
